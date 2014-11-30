e614a8919d4933ab71eac721ba2c306cfdd70913
Brett <brettstanaland86@gmail.com>
2014-11-30 10:38:28 -0500

    this makes the queue page work
    work is such a loosely used word these days.... fuck you php

--- htdocs/fpga/pages/queue.php
+++ htdocs/fpga/pages/queue.php
@@ -7,51 +7,50 @@ page and access the board at a time.-->
 
 <html>
     <head>
+		<meta charset="utf-8">
     	<title>Queue</title>
+		
     </head>
-    <body onunload="Ragequit()">
+    <body>
     	Please wait, you are in the queue to use the board.
     </body>
 </html>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script>
-    $(document).ready(function () {
-        <?php 
+
+     window.onload = function () {
+		
+		<?php 
             //gets new id and current user id from db
 	    $connect = mysql_connect("localhost", "root","") or die("Couldn't Connect");
-	    mysql_select_db("brittlemess") or die("Couldn't find DB");
-
-	    $query = mysql_query("SELECT * FROM queue");
-	    while($output = mysql_fetch_assoc($query)){
-		$userId = $output['NextId'];
-		$beingServed = $output['BeingServed'];
-            }
-
-	    $query = mysql_query("UPDATE queue SET UserId = '" . ++$userId . "'");
-
-    	    while($userId != $beingServed){
-    		$query = mysql_query("SELECT * FROM queue");
-	    	while($output = mysql_fetch_assoc($query)){
-		    $beingServed = $output['BeingServed'];
-		}
-    	    }
-	
-	    header("location:../pages/userMain.php");
-        ?>
-    });
-
-    function Ragequit(){
-        <?php
-            //baboo remembers what you did
-            $connect = mysql_connect("localhost", "brittlemess","password") or die("Couldn't Connecy");
 	    mysql_select_db("brittlemess") or die("Couldn't find DB");
 		
 	    $query = mysql_query("SELECT * FROM queue");
 	    while($output = mysql_fetch_assoc($query)){
-		$baboo = $output['Baboo'] + $userId + ",";
-            }    
-
-            $query = mysql_query("UPDATE queue SET Baboo = '" . $baboo . "'");
-        ?>
-    }
+			$userId = $output['NextId'];
+			$beingServed = $output['BeingServed'];
+        }
+		
+		$newId = $userId + 1;
+		$query = mysql_query("UPDATE queue SET NextId = '" . $newId . "'");
+		if($userId == $beingServed){
+			header("location:../pages/userMain.php");
+		}
+        ?>   
+		
+	}
+	
+	window.setInterval(function(){
+		location.reload();
+	}, 10000);
+	
+	window.addEventListener("beforeunload", function (e) {
+		<?php
+		//baboo remembers what you did
+		$connect = mysql_connect("localhost", "root","") or die("Couldn't Connect");
+		mysql_select_db("brittlemess") or die("Couldn't find DB"); 
+		$query = mysql_query("INSERT INTO baboo (RagequitId) VALUES ('" . $userId . "')");
+		?>
+	});
 </script>
